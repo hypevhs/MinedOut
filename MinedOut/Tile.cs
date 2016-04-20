f439f7e7ab89f9369e6141e02642b2008993e7a3
libjared <libjared@users.noreply.github.com>
2016-04-20 03:14:02 -0400

    add Tile

---
+++ MinedOut/Tile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace MinedOut
+{
+    internal abstract class Tile : IGameDrawable
+    {
+        private int X { get; }
+        private int Y { get; }
+
+        protected Tile(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public List<DrawCommand> Draw()
+        {
+            return new List<DrawCommand>
+            {
+                new DrawCommand(X, Y, '*', Color.Red, Color.Cyan)
+            };
+        }
+    }
+
+    internal class MineTile : Tile
+    {
+        public MineTile(int x, int y) : base(x, y)
+        {
+        }
+    }
+}
