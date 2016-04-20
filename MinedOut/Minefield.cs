cc74003f71ba3709964ba7f912b6263d414e2815
libjared <libjared@users.noreply.github.com>
2016-04-20 03:14:15 -0400

    add Minefield

---
+++ MinedOut/Minefield.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace MinedOut
+{
+    internal class Minefield : IGameDrawable
+    {
+        private const int SizeX = 23;
+        private const int SizeY = 16;
+
+        private readonly Tile[,] tiles;
+
+        public Minefield()
+        {
+            tiles = new Tile[SizeX, SizeY];
+
+            for (var y = 0; y < SizeY; y++)
+            {
+                for (var x = 0; x < SizeX; x++)
+                {
+                    tiles[x, y] = new MineTile(x, y);
+                }
+            }
+        }
+
+        public List<DrawCommand> Draw()
+        {
+            var cmds = new List<DrawCommand>();
+            for (var y = 0; y < SizeY; y++)
+            {
+                for (var x = 0; x < SizeX; x++)
+                {
+                    var tile = tiles[x, y];
+                    cmds.AddRange(tile.Draw());
+                }
+            }
+            return cmds;
+        }
+    }
+}
