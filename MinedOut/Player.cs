6a03701cb521c14e1d2420435a8efdd0859a63bf
libjared <libjared@users.noreply.github.com>
2016-05-03 15:33:01 -0400

    (fix build) walking around

--- MinedOut/Player.cs
+++ MinedOut/Player.cs
@@ -38,6 +38,34 @@ namespace MinedOut
             }
         }
 
+        private void UpdatePosition()
+        {
+            //get next pos
+            var nextPosX = X + (ControlsMoveRt ? 1 : 0) + (ControlsMoveLf ? -1 : 0);
+            var nextPosY = Y + (ControlsMoveDn ? 1 : 0) + (ControlsMoveUp ? -1 : 0);
+
+            //move if no wall blocking our way
+            if (CanWalk(nextPosX, nextPosY))
+            {
+                X = nextPosX;
+                Y = nextPosY;
+            }
+        }
+
+        private bool CanWalk(int nextPosX, int nextPosY)
+        {
+            return nextPosX >= 0 && nextPosX < Minefield.SizeX
+                && nextPosY >= 0 && nextPosY < Minefield.SizeY;
+        }
+
+        private void GetControls()
+        {
+            ControlsMoveUp = Keyboard.IsKeyPressed(Keyboard.Key.Up);
+            ControlsMoveDn = Keyboard.IsKeyPressed(Keyboard.Key.Down);
+            ControlsMoveLf = Keyboard.IsKeyPressed(Keyboard.Key.Left);
+            ControlsMoveRt = Keyboard.IsKeyPressed(Keyboard.Key.Right);
+        }
+
         public void Draw(DrawCommandCollection drawCmds)
         {
             var drawCmd = new DrawCommand(X, Y, (char)0x02, Color.Red, groundColor);
