e64ba251ac392c557a6a29490b2f1b9117895a42
libjared <libjared@users.noreply.github.com>
2016-05-03 19:05:53 -0400

    randomprovider

---
+++ MinedOut/RandomProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinedOut
+{
+    internal static class RandomProvider
+    {
+        public static Random Random { get; }
+
+        static RandomProvider()
+        {
+            Random = new Random();
+        }
+    }
+}
