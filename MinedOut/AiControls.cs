12c5e1c50efc6a52bbbbdaca90f26122b12f4ab8
libjared <libjared@users.noreply.github.com>
2016-05-06 06:27:15 -0400

    refactor, like extract Pathfinder class

--- MinedOut/AiControls.cs
+++ MinedOut/AiControls.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using SFML.Graphics;
-using SFML.System;
 using SFML.Window;
 
 namespace MinedOut
@@ -26,11 +24,13 @@ namespace MinedOut
             this.plr = plr;
             this.field = field;
             explorePls = new HashSet<DrawableTile>();
+            pathfinder = new Pathfinder(plr, field);
         }
 
         private bool lastAdvance;
         private bool currAdvance;
         private bool oddFrame;
+        private readonly Pathfinder pathfinder;
 
         public void Update()
         {
@@ -50,21 +50,6 @@ namespace MinedOut
             }
         }
 
-        public void Draw(DrawCommandCollection drawCmds)
-        {
-            //draw the lists for debug purposes
-            foreach (var expl in explorePls)
-            {
-                drawCmds.Add(new DrawCommand(expl.X, expl.Y, '#', Color.Yellow, Color.Transparent));
-            }
-
-            if (cachedPath != null)
-            {
-                var str = string.Join(", ", cachedPath);
-                drawCmds.AddRange(DrawCommand.FromString(0, 0, str, Color.Yellow, Color.Black));
-            }
-        }
-
         private void ResetControls()
         {
             MoveUp = false;
@@ -75,122 +60,6 @@ namespace MinedOut
             FlagDn = false;
             FlagLf = false;
             FlagRt = false;
-        }
-
-        private void UpdateControls()
-        {
-            var exploreTarget = GetExploreTarget();
-            if (exploreTarget == null)
-            {
-                //meaning: no more tiles to check.
-                FlagRt = true;
-                return;
-            }
-
-            MoveTowardTarget(exploreTarget);
-        }
-
-        private void MoveTowardTarget(DrawableTile exploreTarget)
-        {
-            var nextStep = NextStepInMovingTowards(exploreTarget);
-
-            //can reach our target in one step? remove it from todolist, then move to it
-            var tileTmp = new GroundTile(nextStep.X, nextStep.Y);
-            if (tileTmp.DistanceTo(plr.X, plr.Y) == 1)
-            {
-                var diffX = nextStep.X - plr.X;
-                var diffY = nextStep.Y - plr.Y;
-                if (diffX == 1) MoveRt = true;
-                if (diffX == -1) MoveLf = true;
-                if (diffY == 1) MoveDn = true;
-                if (diffY == -1) MoveUp = true;
-            }
-            else
-            {
-                //meaning: the distance is not 1 so it's a pathfinding error probably
-                FlagDn = true;
-            }
-        }
-
-        private DrawableTile GetExploreTarget()
-        {
-            //get explore request, sorted by distance ascending
-            var hasADuggedPathToIt = explorePls.Where(HasADuggedPathToIt).ToList();
-            var sortedExplore = hasADuggedPathToIt.OrderBy(tile => tile.DistanceTo(plr.X, plr.Y)).ToList();
-            var topPriority = sortedExplore.FirstOrDefault();
-            return topPriority;
-        }
-
-        private DrawableTile cachedPathDest;
-        private List<DijkstraVertex> cachedPath;
-
-        private Vector2i NextStepInMovingTowards(DrawableTile exploreHere)
-        {
-            if (exploreHere != cachedPathDest)
-            {
-                //need to regen path
-                cachedPath = Dijkstra(new Vector2i(exploreHere.X, exploreHere.Y));
-                cachedPathDest = exploreHere;
-            }
-            //now that we've cached the path, pop the first step and return it
-            var firstVertex = cachedPath.First();
-            cachedPath.Remove(firstVertex);
-            return firstVertex.ToVector2i();
-        }
-
-        private List<DijkstraVertex> Dijkstra(Vector2i targetPos)
-        {
-            DijkstraVertex target = new DijkstraVertex(targetPos.X, targetPos.Y);
-
-            IEnumerable<DijkstraVertex> allDug = PositionsOfAllDuggedTiles().Select(t => new DijkstraVertex(t.X, t.Y));
-            var graphAsList = allDug.Concat(new List<DijkstraVertex> { target });
-            IEnumerable<DijkstraVertex> graph = new List<DijkstraVertex>(graphAsList);
-
-            DijkstraVertex source = graph.First(v => v.X == plr.X && v.Y == plr.Y);
-
-            foreach (DijkstraVertex v in graph)
-            {
-                v.Dist = 99999;
-                v.Previous = null;
-            }
-            source.Dist = 0;
-            var Q = new List<DijkstraVertex>(graph.ToList());
-            while (Q.Count != 0)
-            {
-                DijkstraVertex u = Q.OrderBy(t => t.Dist).First();
-                Q.Remove(u);
-                IEnumerable<DijkstraVertex> vs = GetNeighbors(graph, u);
-                foreach (DijkstraVertex v in vs)
-                {
-                    int alt = u.Dist + u.DistanceTo(v);
-                    if (alt < v.Dist)
-                    {
-                        v.Dist = alt;
-                        v.Previous = u;
-                    }
-                }
-            }
-
-            var path = target.GetPathFromSource();
-            return path;
-        }
-
-        private IEnumerable<DijkstraVertex> GetNeighbors(IEnumerable<DijkstraVertex> graph, DijkstraVertex dijkstraVertex)
-        {
-            return graph.Where(t => t.DistanceTo(dijkstraVertex) == 1);
-        }
-
-        private IEnumerable<Vector2i> PositionsOfAllDuggedTiles()
-        {
-            var allDugTiles = field.GetAllTiles().Where(t => t.Dug);
-            var positions = allDugTiles.Select(t => new Vector2i(t.X, t.Y));
-            return positions;
-        }
-
-        private bool HasADuggedPathToIt(DrawableTile targetTile)
-        {
-            var surrounding = field.GetCardinalAdjacent(targetTile.X, targetTile.Y);
-            return surrounding.Any(t => t.Dug);
         }
 
         private void UpdatePriorities()
@@ -211,14 +80,70 @@ namespace MinedOut
             {
                 var adjUnexplored =
                     field.GetAdjacent(mcmWhereX, mcmWhereY)
-                    .Where(t => t.Dug == false);
-                PleaseExplore(adjUnexplored);
+                        .Where(t => t.Dug == false);
+                explorePls.UnionWith(adjUnexplored);
             }
         }
 
-        private void PleaseExplore(IEnumerable<DrawableTile> where)
+        private DrawableTile GetAnExploreTarget()
         {
-            explorePls.UnionWith(where);
+            //get explore request, sorted by distance ascending
+            var hasADuggedPathToIt = explorePls.Where(HasADuggedPathToIt).ToList();
+            var sortedExplore = hasADuggedPathToIt.OrderBy(tile => tile.DistanceTo(plr.X, plr.Y)).ToList();
+            var topPriority = sortedExplore.FirstOrDefault();
+            return topPriority;
+        }
+
+        private void MoveTowardTarget(DrawableTile exploreTarget)
+        {
+            var nextStep = pathfinder.NextStepInMovingTowards(exploreTarget);
+
+            //can reach our target in one step? remove it from todolist, then move to it
+            var tileTmp = new GroundTile(nextStep.X, nextStep.Y);
+            if (tileTmp.DistanceTo(plr.X, plr.Y) == 1)
+            {
+                var diffX = nextStep.X - plr.X;
+                var diffY = nextStep.Y - plr.Y;
+                if (diffX == 1) MoveRt = true;
+                if (diffX == -1) MoveLf = true;
+                if (diffY == 1) MoveDn = true;
+                if (diffY == -1) MoveUp = true;
+            }
+            else
+            {
+                //meaning: the distance is not 1 so it's a pathfinding error probably
+                FlagDn = true;
+            }
+        }
+        
+        private void UpdateControls()
+        {
+            var exploreTarget = GetAnExploreTarget();
+            if (exploreTarget == null)
+            {
+                //meaning: no more tiles to check.
+                FlagRt = true;
+                return;
+            }
+
+            MoveTowardTarget(exploreTarget);
+        }
+
+        private bool HasADuggedPathToIt(DrawableTile targetTile)
+        {
+            var surrounding = field.GetCardinalAdjacent(targetTile.X, targetTile.Y);
+            return surrounding.Any(t => t.Dug);
+        }
+
+        public void Draw(DrawCommandCollection drawCmds)
+        {
+            //draw the lists for debug purposes
+            foreach (var expl in explorePls)
+            {
+                drawCmds.Add(new DrawCommand(expl.X, expl.Y, '#', Color.Yellow, Color.Transparent));
+            }
+
+            pathfinder.Draw(drawCmds);
         }
     }
 }
--- MinedOut/Pathfinding.cs
+++ MinedOut/Pathfinding.cs
@@ -1,12 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SFML.Graphics;
 using SFML.System;
 
 namespace MinedOut
 {
+    internal class Pathfinder
+    {
+        private readonly AiPlayer plr;
+        private readonly Minefield field;
+        private DrawableTile cachedPathDest;
+        private List<DijkstraVertex> cachedPath;
+
+        public Pathfinder(AiPlayer plr, Minefield field)
+        {
+            this.plr = plr;
+            this.field = field;
+        }
+
+        private List<DijkstraVertex> Dijkstra(Vector2i targetPos)
+        {
+            DijkstraVertex target = new DijkstraVertex(targetPos.X, targetPos.Y);
+
+            IEnumerable<DijkstraVertex> allDug = Enumerable.Select<Vector2i, DijkstraVertex>(PositionsOfAllDuggedTiles(), t => new DijkstraVertex(t.X, t.Y));
+            var graphAsList = allDug.Concat(new List<DijkstraVertex> { target });
+            IEnumerable<DijkstraVertex> graph = new List<DijkstraVertex>(graphAsList);
+
+            DijkstraVertex source = graph.First(v => v.X == plr.X && v.Y == plr.Y);
+
+            foreach (DijkstraVertex v in graph)
+            {
+                v.Dist = 99999;
+                v.Previous = null;
+            }
+            source.Dist = 0;
+            var Q = new List<DijkstraVertex>(graph.ToList());
+            while (Q.Count != 0)
+            {
+                DijkstraVertex u = Q.OrderBy(t => t.Dist).First();
+                Q.Remove(u);
+                IEnumerable<DijkstraVertex> vs = GetNeighbors(graph, u);
+                foreach (DijkstraVertex v in vs)
+                {
+                    int alt = u.Dist + u.DistanceTo(v);
+                    if (alt < v.Dist)
+                    {
+                        v.Dist = alt;
+                        v.Previous = u;
+                    }
+                }
+            }
+
+            var path = target.GetPathFromSource();
+            return path;
+        }
+
+        private IEnumerable<DijkstraVertex> GetNeighbors(IEnumerable<DijkstraVertex> graph, DijkstraVertex dijkstraVertex)
+        {
+            return graph.Where(t => t.DistanceTo(dijkstraVertex) == 1);
+        }
+
+        private IEnumerable<Vector2i> PositionsOfAllDuggedTiles()
+        {
+            var allDugTiles = field.GetAllTiles().Where(t => t.Dug);
+            var positions = allDugTiles.Select(t => new Vector2i(t.X, t.Y));
+            return positions;
+        }
+
+        public Vector2i NextStepInMovingTowards(DrawableTile exploreHere)
+        {
+            if (exploreHere != cachedPathDest)
+            {
+                //need to regen path
+                cachedPath = Dijkstra(new Vector2i(exploreHere.X, exploreHere.Y));
+                cachedPathDest = exploreHere;
+            }
+            //now that we've cached the path, pop the first step and return it
+            var firstVertex = cachedPath.First();
+            cachedPath.Remove(firstVertex);
+            return firstVertex.ToVector2i();
+        }
+
+        public void Draw(DrawCommandCollection drawCmds)
+        {
+            if (cachedPath != null)
+            {
+                var str = string.Join(", ", cachedPath);
+                drawCmds.AddRange(DrawCommand.FromString(0, 0, str, Color.Yellow, Color.Black));
+            }
+        }
+    }
+
     internal class DijkstraVertex : Tile
     {
         public DijkstraVertex Previous { get; set; }
